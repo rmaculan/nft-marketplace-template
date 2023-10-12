005f79bc5577a97451816bc975baa6a75490964e
rmaculan <120978811+rmaculan@users.noreply.github.com>
2023-10-12 18:30:57 -0400

    card implementation

--- screens/Home.js
+++ screens/Home.js
@@ -1,24 +1,60 @@
-import { View, FlatList, Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import React, { useState } from "react";
+import { View, SafeAreaView, FlatList } from "react-native";
 
-import { COLORS, NFTData } from "../constants";
-import FocusedStatusBar from "../components/FocusedStatusBar";
+// import { NFTCard, HomeHeader, FocusedStatusBar } from "../components";
+import NFTCard from "../components/NFTCard";
 import HomeHeader from "../components/HomeHeader";
+import FocusedStatusBar from "../components/FocusedStatusBar";
+import { COLORS, NFTData } from "../constants";
 
 const Home = () => {
+  const [nftData, setNftData] = useState(NFTData);
+
+  const handleSearch = (value) => {
+    if (value.length === 0) {
+      setNftData(NFTData);
+    }
+
+    const filteredData = NFTData.filter((item) =>
+      item.name.toLowerCase().includes(value.toLowerCase())
+    );
+
+    if (filteredData.length === 0) {
+      setNftData(NFTData);
+    } else {
+      setNftData(filteredData);
+    }
+  };
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <FocusedStatusBar backgroundColor={COLORS.primary} />
-
       <View style={{ flex: 1 }}>
         <View style={{ zIndex: 0 }}>
           <FlatList
-            data={NFTData}
-            renderItem={({ item }) => <Text>{item.name}</Text>}
+            data={nftData}
+            renderItem={({ item }) => <NFTCard data={item} />}
+            keyExtractor={(item) => item.id}
+            showsVerticalScrollIndicator={false}
+            ListHeaderComponent={<HomeHeader onSearch={handleSearch} />}
           />
         </View>
+
+        <View
+          style={{
+            position: "absolute",
+            top: 0,
+            bottom: 0,
+            right: 0,
+            left: 0,
+            zIndex: -1,
+          }}
+        >
+          <View
+            style={{ height: 300, backgroundColor: COLORS.primary }} />
+          <View style={{ flex: 1, backgroundColor: COLORS.white }} />
+        </View>
       </View>
-      <HomeHeader />
     </SafeAreaView>
   );
 };
